9a8bb1ad03326b28b4240c2a490b7d8253807aef
IsisTD <220126@fppiramide.com>
2023-09-26 20:06:44 +0200

    update

--- Actividad.js
+++ Actividad.js
@@ -1,1 +1,15 @@
-console.log(13);
+
+
+let romboStr = "";
+
+            for (let i = 1; i <= 9; i += 2) {
+                const espacios = " ".repeat((9 - i) / 2);
+                const asteriscos = "*".repeat(i);
+                romboStr += espacios + asteriscos + "\n";
+            }
+
+            for (let i = 9 - 2; i >= 1; i -= 2) {
+                const espacios = " ".repeat((9 - i) / 2);
+                const asteriscos = "*".repeat(i);
+                romboStr += espacios + asteriscos + "\n";
+            }
