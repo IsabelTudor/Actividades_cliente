b97c5336a2c9cff3e3d6f903de839d08eefed12a
IsisTD <220126@fppiramide.com>
2023-10-09 11:49:43 +0200

    update

--- Actividad.js
+++ Actividad.js
@@ -232,18 +232,22 @@ let contadorNumerosPares=numeros.reduce((cuantosPares,valorActual)=>{
 const palabras = ["manzana", "gato", "sol", "montaña", "libro", "ciudad", "agua", "casa", "perro", "avión"];
 
 let palabraLarga=palabras.find()
-*/
+
 
 //To do: hacer los ejercicios de arrays
 
 //Ej 27
 
-const x= Math.ceil(Math.sqrt(2));
+const x= Math.round(Math.sqrt(69));
 
 console.log(x);
 
-document.write(`El redonde que hemos utilizado es ceil ${x}`)
+document.write(`El redonde que hemos utilizado es round ${x}`)
+*/
+//Ej 28 
 
+const y=Math.random()*5;
+console.log(y,`Esta entre el rango 0 y 5`);
 
 
 
