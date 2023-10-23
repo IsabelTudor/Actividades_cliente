f9443ba1820fa5ad8702ea4d07fd1fc321b5941e
IsisTD <220126@fppiramide.com>
2023-10-23 21:50:30 +0200

    update

--- Actividad.js
+++ Actividad.js
@@ -263,46 +263,67 @@ console.log(valoresSet);
 
 
 //Ej 26
-const paises = ["México", "Japón", "Italia", "Australia", "Brasil", "Canadá", "Francia", "Alemania", "India", "Argentina", "España", "China", "Sudáfrica", "Rusia", "Corea del Sur", "Reino Unido", "Estados Unidos", "Indonesia", "Egipto", "Turquía"];
 
-mostrarArray(paises);
-mostrarArrayAlReves(paises)
-añadirElemento(paises)
-
-function mostrarArray(Array){
-    document.write(`Hay ${Array.length} paises`); 
-    document.write(`<ol>`);
-
-
-    for(let i=0;i< Array.length;i++){
-  
-        document.write(`<li>${Array[i]}</li>`);    
     
-    }
-    document.write(`<ol>`);  
-}
-
-function mostrarArrayAlReves(Array){
-    Array.reverse();
-
-    document.write(`<ol>`);
-
-    for(let i=0;i<Array.length;i++){
-        document.write(`<li>${Array[i]}</li>`);
-    }
-    document.write(`</ol>`);
-}
-
-function añadirElemento(Array){
-    let paisNuevo=prompt("Añada un país");
-    let posicion=parseInt(prompt("Indique la posición"));
-
-    Array.splice(posicion,0,paisNuevo);
-    console.log(Array);
-
-
-}
-
+  const paises = ["México", "Japón", "Italia", "Australia", "Brasil", "Canadá", "Francia", "Alemania", "India", "Argentina", "España", "China", "Sudáfrica", "Rusia", "Corea del Sur", "Reino Unido", "Estados Unidos", "Indonesia", "Egipto", "Turquía"];
+  
+  function mostrarArray(Array) {
+      document.write(`Hay ${Array.length} países`);
+      document.write("<ol>");
+      
+      for (let i = 0; i < Array.length; i++) {
+          document.write(`<li>${Array[i]}</li>`);
+      }
+      
+      document.write("</ol>");
+  }
+  
+  function mostrarArrayAlReves(Array) {
+      const reversedArray = Array.slice().reverse();
+      document.write("<ol>");
+      
+      for (let i = 0; i < reversedArray.length; i++) {
+          document.write(`<li>${reversedArray[i]}</li>`);
+      }
+      
+      document.write("</ol>");
+  }
+  
+  function añadirElemento(Array) {
+      let paisNuevo = prompt("Añada un país");
+      let posicion = parseInt(prompt("Indique la posición"));
+      
+      if (!isNaN(posicion) && posicion >= 0 && posicion <= Array.length) {
+          Array.splice(posicion, 0, paisNuevo);
+          mostrarArray(Array);
+      } else {
+          document.write("Posición no válida");
+      }
+  }
+  
+  function eliminarElementoPaises(Array) {
+      let posicion = parseInt(prompt("Indique la posición que quieres borrar"));
+      
+      if (!isNaN(posicion) && posicion >= 0 && posicion < Array.length) {
+          Array.splice(posicion, 1);
+          mostrarArray(Array);
+      } else {
+          document.write("Posición no válida");
+      }
+  }
+  
+  function buscarPais(Array) {
+      let paisBuscado = prompt("Introduce el país que quieras buscar");
+      const posicion = Array.indexOf(paisBuscado);
+      
+      if (posicion !== -1) {
+          document.write(`El país "${paisBuscado}" se encuentra en la posición ${posicion}`);
+      } else {
+          document.write(`El país "${paisBuscado}" no se encontró en la lista`);
+      }
+  }
+  
+  
 
 
 //Ej 27
@@ -318,13 +339,205 @@ document.write(`El redonde que hemos utilizado es round ${x}`)
 const y=Math.random()*5;
 console.log(y,`Esta entre el rango 0 y 5`);
 
-*/
+
 
 //Ej 29
 
+function calcularTrigonometria(angulo, funcionTrigonometrica) {
+    let resultado;
+
+    switch (funcionTrigonometrica) {
+        case 'sen':
+            resultado = Math.sin(angulo * (Math.PI / 180));
+            document.write(`El seno de ${angulo} grados es ${resultado}`);
+            break;
+        case 'cos':
+            resultado = Math.cos(angulo * (Math.PI / 180));
+            document.write(`El coseno de ${angulo} grados es ${resultado}`);
+            break;
+        case 'tan':
+            resultado = Math.tan(angulo * (Math.PI / 180));
+            document.write(`La tangente de ${angulo} grados es ${resultado}`);
+            break;
+        default:
+            document.write('Función trigonométrica no válida');
+    }
+    
+    return resultado;
+}
+
+// Ejemplo de uso:
+const angulo = 25 // Ángulo en grados
+const funcion = 'cos'; // Función trigonométrica (sen, cos, tan)
+
+const resultado = calcularTrigonometria(angulo, funcion);
 
 
+//Ej 30
+
+let date = new Date();
+console.log(date.toLocaleDateString());
+console.log(date.toDateString());
+console.log(date.toUTCString());
 
 
+//Ej 31
+
+function mostrarHoraActual() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+  
+    // Formato 14:35:07 (hora detallada con minutos y segundos)
+    const horaDetallada = `${formatoNumero(hours)}:${formatoNumero(minutes)}:${formatoNumero(seconds)}`;
+  
+    // Formato 02:35 PM o 02:35 AM (hora con minutos y AM o PM)
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const horaAmPm = `${formatoNumero(getHoraAmPm(hours))}:${formatoNumero(minutes)} ${ampm}`;
+  
+    console.log(`Hora Detallada: ${horaDetallada}`);
+    console.log(`Hora AM/PM: ${horaAmPm}`);
+  }
+  
+  function formatoNumero(numero) {
+    return numero < 10 ? `0${numero}` : numero.toString();
+  }
+  
+  function getHoraAmPm(hours) {
+    return hours > 12 ? hours - 12 : hours;
+  }
+  
+  mostrarHoraActual();
+  
+
+
+//Ej 32
+
+const date = new Date();
+
+const diaActual = date.getDay();
+const mesActual = date.getMonth() + 1; // Sumar 1 para obtener el mes en formato común
+
+const diaFin = 23;
+const mesFin = 6;
+
+const restaDias = diaFin - diaActual;
+const restaMes = mesFin - mesActual;
+
+console.log(`Quedan ${restaDias} días y ${restaMes} meses`);
+
+//Ej 33
+
+function cumpleañosEnDomingos(dia, mes) {
+    const añoActual = new Date().getFullYear();
+    let año = añoActual;
+    const añosEnDomingo = [];
+  
+    while (año <= 2100) {
+      const fechaCumpleaños = new Date(año, mes - 1, dia); // mes - 1 porque los meses se cuentan desde 0 en JavaScript
+      const diaSemana = fechaCumpleaños.getDay(); // 0 para domingo, 1 para lunes, etc.
+  
+      if (diaSemana === 0) {
+        añosEnDomingo.push(año);
+      }
+  
+      año++;
+    }
+  
+    return añosEnDomingo;
+  }
+  
+  const diaCumple = prompt("Introduce el día de tu cumpleaños (1-31):");
+  const mesCumple = prompt("Introduce el mes de tu cumpleaños (1-12):");
+  
+  if (diaCumple && mesCumple) {
+    const añosEnDomingo = cumpleañosEnDomingos(parseInt(diaCumple), parseInt(mesCumple));
+  
+    if (añosEnDomingo.length > 0) {
+      console.log(`Tu cumpleaños caerá en domingo en los siguientes años:`);
+      console.log(añosEnDomingo.join(", "));
+    } else {
+      console.log("Lo siento, tu cumpleaños no caerá en domingo en ningún año hasta el 2100.");
+    }
+  } else {
+    console.log("Debes ingresar un día y un mes válidos.");
+  }
+  
+
+  //Ej 34
+
+  const date=new Date();
+
+  if(date.getDay()==0){
+    console.log(`SU`);
+  }else if(date.getDay()==1){
+    console.log(`MO`);
+  }else if(date.getDay()==2){
+    console.log(`TU`);
+  }else if(date.getDay()==3){
+    console.log(`WE`);
+  }else if(date.getDay()==4){
+    console.log(`TH`);
+  }else if(date.getDay()==5){
+    console.log(`FR`);
+  }else if(date.getDay()==6){
+    console.log(`SA`);
+  }
+  
+ //Ej 35
+
+ function esAñoBisiesto(año) {
+    // Un año bisiesto es divisible por 4, pero no por 100, o es divisible por 400.
+    return (año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0);
+  }
+  
+  function ultimoDiaDelMes(año, mes) {
+    if (mes < 1 || mes > 12) {
+      return null; // Mes inválido
+    }
+  
+    if (mes === 2) {
+      // Febrero
+      if (esAñoBisiesto(año)) {
+        return 29; // Año bisiesto
+      } else {
+        return 28; // No es bisiesto
+      }
+    } else if ([4, 6, 9, 11].includes(mes)) {
+      return 30; // Meses con 30 días
+    } else {
+      return 31; // Meses con 31 días
+    }
+  }
+  
+  // Ejemplo de uso:
+  const año = 2023;
+  const mes = 2;
+  
+  const ultimoDia = ultimoDiaDelMes(año, mes);
+  
+  if (ultimoDia !== null) {
+    console.log(`El último día de ${mes}/${año} es el día ${ultimoDia}.`);
+  } else {
+    console.log("Mes o año inválido.");
+  }
+  */
+ //Ej 36
+ function segundosDesdeComienzoDelDia() {
+    const ahora = new Date();
+    const horas = ahora.getHours();
+    const minutos = ahora.getMinutes();
+    const segundos = ahora.getSeconds();
+    
+    const segundosTotales = (horas * 3600) + (minutos * 60) + segundos;
+  
+    return segundosTotales;
+  }
+  
+  // Ejemplo de uso:
+  const segundos = segundosDesdeComienzoDelDia();
+  console.log(`Han transcurrido ${segundos} segundos desde el comienzo del día.`);
+  
 
 
